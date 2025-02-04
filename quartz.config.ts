7b468ad9a62e7e1faf7ec3c964dfc81d8a606291
Matías <matiasthomasster@gmail.com>
2025-02-03 23:32:26 -0300

    Quartz sync: Feb 3, 2025, 11:32 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "es-ES",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "matiasthomas.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
